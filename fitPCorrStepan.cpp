66e007b982361a0d17b8defdea0db5fcad92771f
rodmendezp <rodmendezp@gmail.com>
2016-11-03 21:49:07 -0300

    Fixed hWextra since was adding the same events

--- fitPCorrStepan.cpp
+++ fitPCorrStepan.cpp
@@ -202,7 +202,7 @@ void FitPCorrStepan::fitHists(Bool_t setInitParams = false)
 
     for(Int_t i = 0; i < nSect; i++){
         f1[i] = new TF1(Form("f1_phi%d", i+1), "[0]+[1]*x+[2]*x*x", phiMin[i], phiMax[i]);
-        f2[i] = new TF1(Form("f2_phi%d", i+1), "[0]+([1]+[2]*x+[3]*x*x)*exp(-x)", 17, 34);
+        f2[i] = new TF1(Form("f2_phi%d", i+1), "[0]+([1]+[2]*x+[3]*x*x)*exp(-x)", thMin, thMax);
     }
 
     if(setInitParams & fexists("initFitParams.txt")){
